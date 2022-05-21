da2cf54a054e166322eddad2f60f3eae7b00757b
Marcos NC <marcosnc@gmail.com>
2022-05-21 12:15:56 -0400

    C3 sdk changes for wormhole integration.

--- src/_example.ts
+++ src/_example.ts
@@ -1,3 +1,4 @@
+import { C3RequestOp, CEDepositRequest } from "./C3RequestTypes"
 import { C3Sdk, connectC3 } from "./C3Sdk"
 import { Signer } from "./Signer"
 
@@ -6,13 +7,27 @@ import { Signer } from "./Signer"
 
 (async () => {
     const signer: Signer = new Signer()
+    const wallet = signer.addFromMnemonic("expand multiply humble vault pulp priority size project dish bamboo hard eternal duty beyond undo below trigger paddle minimum soap quality oval laptop ability toddler")
+
     const sdk: C3Sdk = await connectC3(
         "https://beta-api.c3.io/",
-        "https://node.testnet.algoexplorerapi.io", 80, "",
+        "https://node.testnet.algoexplorerapi.io", 443, "",
         signer.callback,
         signer.tealCallback)
+
     const assets = await sdk.getAssets()
     console.log("C3 Assets:", assets)
+
+    const deposit: CEDepositRequest = {
+        op: C3RequestOp.CE_Deposit,
+        assetId: 0,
+        amount: BigInt(1000)
+    }
+    const userProxy = await sdk.createUserProxy(wallet)
+    console.log("User Proxy: ", userProxy.address())
+    const result = await sdk.performC3Op(deposit, userProxy)
+    console.log("Result", result)
+
 })()
 .then(() => console.log("DONE!"))
 .catch(err => console.log("ERROR!!!", err))
